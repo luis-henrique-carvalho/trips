4e57f828cb305d86d78e9bb50d1b727b2953be9e
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2023-07-07 19:15:29 -0300

    :spakrles: feat: add TripLocation component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -4,6 +4,7 @@ import TripHeader from "./components/TripHeader";
 import TripReservations from "./components/TripReservations";
 import TripDescription from "./components/TripDescription";
 import TripHighlights from "./components/TripHighlights";
+import TripLocation from "./components/TripLocation";
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -26,6 +27,10 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
       <TripReservations trip={trip} />
       <TripDescription description={trip.description} />
       <TripHighlights trip={trip} />
+      <TripLocation
+        location={trip.location}
+        locationDescription={trip.locationDescription}
+      />
     </div>
   );
 };
